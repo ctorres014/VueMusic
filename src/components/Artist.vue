01d93b20c67f2e0923934edc1c4e6338da864072
ctorres014 <ctorres014@gmail.com>
2019-01-25 23:30:53 -0300

    Initial commit

---
+++ src/components/Artist.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  li.artist
+    h2: a(:href="artist.url" target="_blank") {{ artist.name }}
+    img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  //   Definimos el nombre del component definido en vue.js
+  name: 'artist',
+  //  Definimos la propiedad que recibe el component, en :bind en vue.js
+  props: [ 'artist' ]
+}
+</script>
+
+<style lang="stylus" scoped>
+li.artist
+  display block
+  margin 10px 0
+</style>
+
+
+
